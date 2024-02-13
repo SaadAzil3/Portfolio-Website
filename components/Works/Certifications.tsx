92374133b9a84c9e5dc4165bdb11cbe4215ca96b
Saad Azil <138615559+SaadAzil3@users.noreply.github.com>
2024-02-13 16:35:13 +0100

    Update Certifications.tsx

--- components/Works/Certifications.tsx
+++ components/Works/Certifications.tsx
@@ -19,7 +19,19 @@ const Certifications = () => {
                     <FaShare
                         className="fa-solid fa-share"
                     />
+                    The Complete Networking Fundamentals Course. Your CCNA start <a className="cert-link" target="_blank" href="https://www.udemy.com/certificate/UC-aad32535-6b9f-4832-a9d3-d188356dc66b/">see Certificate</a>
+                </li>
+                <li>
+                    <FaShare
+                        className="fa-solid fa-share"
+                    />
                     Python (Basic) programming certificate from Hackerrank <a className="cert-link" target="_blank" href="https://www.hackerrank.com/certificates/e1c937e2be84">see Certificate</a>
+                </li>
+                <li>
+                    <FaShare
+                        className="fa-solid fa-share"
+                    />
+                    Advent of Cyber 2023 Certificate Of Completion <a className="cert-link" target="_blank" href="https://tryhackme-certificates.s3-eu-west-1.amazonaws.com/THM-1GTZVFGV6E.png">see Certificate</a>
                 </li>
             </ul>
         </motion.div>
