b743a2fd54ba56646bdee103e6ca2778d8c34b16
Saad Azil <138615559+SaadAzil3@users.noreply.github.com>
2024-06-26 17:11:37 +0100

    Update RightSide.tsx

--- components/RightSide.tsx
+++ components/RightSide.tsx
@@ -5,7 +5,7 @@ const RightSide = () => {
     return (
         <div className="right-side">
             <a href="mailto:azilsaad06@gmail.com">
-                <p className="mail-text">azilsaad06@gmail.com</p>
+                <p className="mail-text">azil.saad@outlook.com</p>
             </a>
             <div className="before-icons"></div>
         </div>
