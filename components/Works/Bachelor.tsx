c8d31e122284e87577562f584e9bdc9f3d33fd4d
Saad Azil <138615559+SaadAzil3@users.noreply.github.com>
2024-09-07 19:59:46 +0100

    Update Bachelor.tsx

--- components/Works/Bachelor.tsx
+++ components/Works/Bachelor.tsx
@@ -12,7 +12,28 @@ const Bachelor = () => {
                 Bachelor Degree <span> @Univ of Bejaia</span>
             </h3>  
             <p>
-                Sep 2021 - Present
+                Sep 2021 - June 2024
+            </p>
+            <ul>
+                <li>
+                    <FaShare
+                        className="fa-solid fa-share"
+                    />
+                    3rd Year Computer Science Student @University Of Bejaia specialized in Network and Security
+                </li>
+            </ul>
+        </motion.div>
+        <motion.div
+            initial= {{opacity: 0}}
+            animate= {{opacity: 1}}
+            transition= {{duration:0.2}}
+            className="first-ex"
+        >
+            <h3 >
+                Master Degree <span> @Univ of Bejaia</span>
+            </h3>  
+            <p>
+                Sep 2024 - Present
             </p>
             <ul>
                 <li>
